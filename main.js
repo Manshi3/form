d30708ac26a405c42b51faef930abbde74bec73b
Manshi <105007863+Manshi3@users.noreply.github.com>
2024-01-16 13:43:53 +0530

    main.js

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ function openPopup() {
     popup.classList.add("open-popup");
 }
 function closePopup() {
-    popup.classList.remove("open-popup");
-}
-function closePopup() {
     const element = document.getElementById("invoice");
     html2pdf()
     .from(element)
